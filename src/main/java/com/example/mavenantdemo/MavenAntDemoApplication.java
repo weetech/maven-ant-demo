869c24a1368467ddb15bb3086a1e482a75ab2386
Jaymin Patel <jaymin@weetechsolution.com>
2022-05-03 12:07:59 +0530

    maven-ant-demo

---
+++ src/main/java/com/example/mavenantdemo/MavenAntDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenantdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAntDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAntDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mavenantdemo/MavenAntDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenantdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenAntDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
